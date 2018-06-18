49b5833269d076e6ce1830193349c15be583d3da
blurbyte <info@blurbyte.com>
2018-06-18 11:07:42 +0200

    Apollo playground setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello!!!');
+import { ApolloServer, gql } from 'apollo-server';
+import chalk from 'chalk';
+
+const typeDefs = gql`
+  "**Test** type"
+  type Hello {
+    message: String!
+  }
+
+  "Root query"
+  type Query {
+    hello: Hello
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => ({ message: 'Hello there!' }),
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(chalk.bgBlue(`🚀  Server ready at ${url}`));
+});

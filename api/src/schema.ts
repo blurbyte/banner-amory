b685551d6f3c7f1bdfa5c22d3e958e24983c588c
blurbyte <info@blurbyte.com>
2018-06-19 12:47:37 +0200

    Create GraphQL root schema

---
+++ api/src/schema.ts
@@ -0,0 +1,42 @@
+// root GraphQL schema
+
+import { gql, makeExecutableSchema } from 'apollo-server';
+import { getCustomRepository } from 'typeorm';
+
+import { ItemRepository } from './repositories/Item';
+
+// types
+import { Item } from './types/Item';
+
+const Root = gql`
+  type Query {
+    items: [Item]
+  }
+`
+
+const rootResolvers = {
+  Query: {
+    items: async () => {
+      const items = await getCustomRepository(ItemRepository).getAllItems();
+      return items;
+    }
+  }
+}
+
+// merged types
+const typeDefs = [
+  Root,
+  Item
+];
+
+// merged resolvers
+const resolvers = {
+  ...rootResolvers
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+export default schema;

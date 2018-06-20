6bef6dfda59ac68c048a3d0dc9ebaf967df970df
blurbyte <info@blurbyte.com>
2018-06-20 23:35:32 +0200

    Add svg icons

---
+++ web/src/components/Icons/Banner.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 90">
+        <path fill="#B62F12" d="M50.005 90L25.003 67.452 0 90V0h50.005v90z" />
+      </svg>
+    );
+  }
+}
+
+export default Banner;
---
+++ web/src/components/Icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as Logo } from './Logo';
+export { default as Banner } from './Banner';
+export { default as Ring } from './Ring';
+export { default as Shield } from './Shield';
+export { default as Magnifier } from './Magnifier';

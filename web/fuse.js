adf3948658a2699a88914ef97a429d6d198e34c1
blurbyte <info@blurbyte.com>
2018-06-20 19:37:50 +0200

    Configure fusebox

---
+++ web/fuse.js
@@ -0,0 +1,51 @@
+const { FuseBox, WebIndexPlugin } = require('fuse-box');
+const { src, task, context } = require('fuse-box/sparky');
+
+// Context availible for all tasks
+context(class {
+  getConfig() {
+    return FuseBox.init({
+      homeDir: 'src',
+      target: 'browser@es5',
+      output: 'dist/$name.js',
+      cache: true,
+      sourceMaps: true,
+      plugins: [
+        WebIndexPlugin({
+          template: 'public/index.html',
+          title: 'Banner Armory'
+        })
+      ]
+    });
+  }
+
+  createBundle(fuse) {
+    // Creates vendor libs bundle
+    fuse.bundle('vendor').instructions('~ index.tsx');
+
+    // Project files bundle
+    const app = fuse.bundle('app');
+
+    app.hmr();
+    app.watch();
+
+    app.instructions('> [index.tsx]');
+
+    return app;
+  }
+});
+
+// Removing dist directory
+task('clean', () => src('dist').clean('dist').exec());
+
+task('default', ['clean', 'copy'], async context => {
+  const fuse = context.getConfig();
+
+  fuse.dev({
+    port: 5000
+  });
+
+  context.createBundle(fuse);
+
+  await fuse.run();
+});

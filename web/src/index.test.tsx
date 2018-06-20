bf22ef5d3c0fe1fc6983a58352d57c600e0a5119
blurbyte <info@blurbyte.com>
2018-06-20 19:38:33 +0200

    Create simple app starting point

---
+++ web/src/index.test.tsx
@@ -0,0 +1,3 @@
+test('should run', () => {
+  expect(1).toEqual(1);
+});
---
+++ web/src/index.tsx
@@ -0,0 +1,8 @@
+// application entry point
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));

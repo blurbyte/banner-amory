b4241e307a7adc7b1a7d199412919a805cba9933
blurbyte <info@blurbyte.com>
2018-06-21 12:39:39 +0200

    Update icons

--- web/src/components/Icons/Banner.tsx
+++ web/src/components/Icons/Banner.tsx
@@ -4,7 +4,7 @@ class Banner extends React.Component {
   render() {
     return (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 90">
-        <path fill="#B62F12" d="M50.005 90L25.003 67.452 0 90V0h50.005v90z" />
+        <path fill="#d92f29" d="M50.005 90L25.003 67.452 0 90V0h50.005v90z" />
       </svg>
     );
   }

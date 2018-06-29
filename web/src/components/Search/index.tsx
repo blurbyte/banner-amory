b208ac210d8affd75ff84c99a67c13d67b2e34b0
blurbyte <info@blurbyte.com>
2018-06-30 01:16:24 +0200

    Implement Search logic

--- web/src/components/Search/index.tsx
+++ web/src/components/Search/index.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
+import { navigate } from '@reach/router';
 
 import Form from './Form';
 import Input from './Input';
 import Button from './Button';
 
-class Search extends React.Component {
+const initialState = {
+  query: ''
+};
+
+type SearchState = Readonly<typeof initialState>;
+
+class Search extends React.Component<{}, SearchState> {
+  readonly state = initialState;
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <Input
           type="search"
           autoCapitalize="off"
@@ -15,11 +24,27 @@ class Search extends React.Component {
           autoCorrect="off"
           autoComplete="off"
           placeholder="Search items"
+          onChange={this.handleChange}
         />
         <Button />
       </Form>
     );
   }
+
+  private handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    this.setState({ query });
+  };
+
+  private handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { query } = this.state;
+    if (query) {
+      // redirect to search location if query is not empty
+      navigate(`/search/${query.trim()}`);
+    }
+  };
 }
 
 export default Search;

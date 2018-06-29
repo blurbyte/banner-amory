e53fa862e8ecd4e83926d97dd3c0468dbf213296
blurbyte <info@blurbyte.com>
2018-06-30 01:41:49 +0200

    Add Search submit test

--- web/src/components/Search/__tests__/Search.test.tsx
+++ web/src/components/Search/__tests__/Search.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { navigate } from '@reach/router';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 
 import Search from '../index';
 
@@ -8,17 +8,29 @@ jest.mock('../../Icons', () => ({
   Magnifier: 'svg'
 }));
 
+// spy on navigate
 jest.mock('@reach/router', () => ({
   navigate: jest.fn()
 }))
 
 beforeEach(cleanup);
 
-test('renders correctly', () => {
+test('submits search correctly', () => {
+  // arrange
   const { container, getByPlaceholderText } = render(<Search />);
 
-  const searchInput = getByPlaceholderText('Search items');
+  const searchInput = getByPlaceholderText('Search items') as HTMLInputElement;
+  searchInput.value = '  alette  ';
+  // for controlled component change event have to be fired manually
+  fireEvent.change(searchInput);
+
   const submitButton = container.querySelector('button');
 
+  // act
+  fireEvent.click(submitButton);
+
+  // assert
+  expect(navigate).toHaveBeenCalledTimes(1);
+  expect(navigate).toHaveBeenCalledWith('/search/alette');
   expect(container.firstChild).toMatchSnapshot();
 });

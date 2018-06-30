5ce79e364f5afbbd8bccac9eb48f6ac2b84dab2a
blurbyte <info@blurbyte.com>
2018-07-01 00:50:50 +0200

    Update ItemsGrid tests

--- web/src/components/ItemsGrid/__tests__/ItemsGrid.test.tsx
+++ web/src/components/ItemsGrid/__tests__/ItemsGrid.test.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
+import { ItemBasic } from '@sharedTypes/Item';
 import ItemsGrid from '../index';
 
 test('renders correctly', () => {
-  const items = [
+  const items: ItemBasic[] = [
     {
+      name: 'Test',
       slug: 'test-slug',
+      gamePart: 2,
       rank: 10
     },
     {
+      name: 'Taylor Swift',
       slug: 'taylor-swift',
+      gamePart: 2,
       rank: 1
     }
   ];

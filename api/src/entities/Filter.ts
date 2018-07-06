80303a9039add81889f52898b26de0b9066350b2
blurbyte <info@blurbyte.com>
2018-07-06 14:13:47 +0200

    Add AvailableFilterBonusProperty type

--- api/src/entities/Filter.ts
+++ api/src/entities/Filter.ts
@@ -2,8 +2,8 @@ import { Item } from './Item';
 
 // 0 means ALL parts and ranks
 export type Filter = {
-  part: 0 | Item['gamePart']
-  rank: 0 | Item['rank']
+  part: 0 | Item['gamePart'];
+  rank: 0 | Item['rank'];
   bonus: FilterBonus;
 };
 
@@ -17,3 +17,27 @@ export enum FilterBonus {
   FromMarket = 'FROM_MARKET',
   Movement = 'MOVEMENT'
 }
+
+// Item properties on which filtering is based
+export type AvailableFilterBonusProperty = Extract<
+  keyof Item,
+  | 'statStrength'
+  | 'statWillpower'
+  | 'statArmor'
+  | 'statArmorBreak'
+  | 'talentAllRanks'
+  | 'talentStrength'
+  | 'talentWillpower'
+  | 'talentArmor'
+  | 'talentArmorBreak'
+  | 'talentExertion'
+  | 'perTurnArmor'
+  | 'perTurnWillpower'
+  | 'onRestArmor'
+  | 'onRestWillpower'
+  | 'chanceCrit'
+  | 'chanceDodge'
+  | 'chanceDivert'
+  | 'acquirementMarketplace'
+  | 'movement'
+>;

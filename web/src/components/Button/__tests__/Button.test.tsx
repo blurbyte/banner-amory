8c39b6048e24ab571b9a2575262eae377f9a2608
blurbyte <info@blurbyte.com>
2018-08-01 12:58:48 +0200

    Add Button tests

---
+++ web/src/components/Button/__tests__/Button.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Button from '../index';
+
+test('renders correctly', () => {
+  const component = renderer.create(<Button type="submit">Submit</Button>);
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('renders correctly with icon', () => {
+  const Icon = () => <>Icon</>;
+  const component = renderer.create(<Button type="submit" icon={Icon}>Submit</Button>);
+  expect(component.toJSON()).toMatchSnapshot();
+});
---
+++ web/src/components/Button/__tests__/Icon.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Icon from '../Icon';
+
+test('renders correctly', () => {
+  const component = renderer.create(<Icon />);
+  expect(component.toJSON()).toMatchSnapshot();
+});
